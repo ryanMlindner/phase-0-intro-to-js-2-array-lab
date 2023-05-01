2b736cfc91ab024805937b58bf02a5f22b264476
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-05-01 16:10:02 -0600

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.splice(2,1);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.splice(0,1);
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name ,...cats];
+}
+
+//terrible readability, but we aren't worrying about that yet
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+// see previous comment
+function removeFirstCat() {
+    return cats.slice(1);
+}
